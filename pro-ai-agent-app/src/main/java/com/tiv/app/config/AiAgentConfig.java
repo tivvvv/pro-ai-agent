30e2975eba06f6f2c72d0d7803282c330de5b521
tivvvv <18612553935@163.com>
2025-08-20 20:35:11 +0800

    feat: 配置ai agent.

---
+++ pro-ai-agent-app/src/main/java/com/tiv/app/config/AiAgentConfig.java
@@ -0,0 +1,40 @@
+package com.tiv.app.config;
+
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * ai agent配置.
+ */
+@Configuration
+public class AiAgentConfig {
+
+    @Bean("vectorStore")
+    public PgVectorStore pgVectorStore(@Value("${spring.ai.openai.base-url}") String baseUrl,
+                                       @Value("${spring.ai.openai.api-key}") String apiKey,
+                                       @Qualifier("pgVectorJdbcTemplate") JdbcTemplate jdbcTemplate) {
+
+        OpenAiApi openAiApi = OpenAiApi.builder()
+                .baseUrl(baseUrl)
+                .apiKey(apiKey)
+                .build();
+
+        OpenAiEmbeddingModel embeddingModel = new OpenAiEmbeddingModel(openAiApi);
+        return PgVectorStore.builder(jdbcTemplate, embeddingModel)
+                .vectorTableName("vector_store_openai")
+                .build();
+    }
+
+    @Bean("tokenTextSplitter")
+    public TokenTextSplitter tokenTextSplitter() {
+        return new TokenTextSplitter();
+    }
+
+}
